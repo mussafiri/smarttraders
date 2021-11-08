d6ff32d6ae7f129e1ed570a262704544e1c9e1ca
Elisante Kamera <elisante@bcmholdings.io>
2021-11-08 14:49:52 +0300

    smartTrader v12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1192,7 +1192,7 @@
     <!-- End reviews Area -->
 
     <!-- Start FAQ area -->
-    <div class="faq-area  area-padding">
+    <div class="faq-area">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
